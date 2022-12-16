33a8d4772524721d165ec3943fdfd089e42e0d41
cinephile <aprilkb412@naver.com>
2022-12-16 11:07:30 +0900

    인풋 자동 포커싱

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,19 +1,28 @@
 import { Content, Title, Input, InputDate, InputTodo, InputBtn } from "./style";
 import { v4 as uuid } from "uuid";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { useDispatch } from "react-redux";
 import { addTodo } from "../../redux/modules/manageTodo";
 
 const Header = () => {
   const dispatch = useDispatch();
 
+  // 인풋 자동 포커싱
+  const focusDate = useRef();
+  const focusTodo = useRef();
+
+  // 인풋 초기화
   const [date, setDate] = useState("");
   const [todo, setTodo] = useState("");
 
   // 투두리스트 추가
   const addBtn = () => {
-    if (!date || !todo) {
-      return alert("빠진 내용이 없나 확인해보세요.");
+    if (!date) {
+      alert("빠진 내용이 없나 확인해보세요.");
+      focusDate.current.focus();
+    } else if (!todo) {
+      alert("빠진 내용이 없나 확인해보세요.");
+      focusTodo.current.focus();
     } else {
       setDate("");
       setTodo("");
@@ -25,6 +34,7 @@ const Header = () => {
           isDone: false,
         })
       );
+      focusDate.current.focus();
     }
   };
 
@@ -35,11 +45,14 @@ const Header = () => {
         <InputDate
           placeholder="date"
           value={date}
+          ref={focusDate}
           onChange={(e) => setDate(e.target.value)}
+          autoFocus
         />
         <InputTodo
           placeholder="Do your job!"
           value={todo}
+          ref={focusTodo}
           onChange={(e) => setTodo(e.target.value)}
         />
         <InputBtn onClick={addBtn}>add</InputBtn>
